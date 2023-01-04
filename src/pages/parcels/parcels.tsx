c6a6192b71f4005bea1069e9367d82af5917fbea
Ilia Timofeev <timofeev.i.v@mail.ru>
2023-01-04 18:47:02 +0300

    backend

--- src/pages/parcels/parcels.tsx
+++ src/pages/parcels/parcels.tsx
@@ -1,9 +1,20 @@
 import { Breadcrumb, Button, Layout, Input } from 'antd';
+import axios from 'axios';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { IState } from '../../store/modules';
+// import {Buffer} from 'buffer'
 // import { setParcels } from '../../store/modules/pages/parcels/actions';
+
+interface ITest {
+  a: string;
+  b: string
+}
+
+interface ITestResponse {
+  data: ITest;
+}
 
 export const Parcels = () => {
 
@@ -13,9 +24,28 @@ export const Parcels = () => {
 
   const [testValue, setTestValue] = React.useState("")
 
+  const url = "http://localhost/api/hs/api/test/gettestinfo"
+
   const dispatch = useDispatch();
-  const data = useSelector((state:IState)=>state.pages.parcels)
-  const handleClick = React.useCallback((payload) => dispatch({ type: 'add', payload}), []) 
+  const data = useSelector((state: IState) => state.pages.parcels)
+  // const username = "admin";
+  // const password = "0000"
+  
+  // const token = Buffer.from(`${username}:${password}`,'utf8').toString('base64');
+
+
+  const handleClick = React.useCallback(
+    async () => {
+      return axios.get(
+        url,
+        {},
+      ).then((response: ITestResponse)=> {
+      console.log(response)
+     return dispatch({ type: 'add', payload: response.data.a})
+    })
+      .catch(err => console.log(err));
+    }, []
+  )
 
   return (
 
@@ -47,9 +77,9 @@ export const Parcels = () => {
         />
         Parcels
         {parcelId}
-        {data.map((el,i)=>(<div key={i}>{el}</div>)) }
+        {data.map((el, i) => (<div key={i}>{el}</div>))}
         <Button
-          onClick={()=>handleClick(testValue)}
+          onClick={() => handleClick()}
         >Установить</Button>
       </Content>
     </Layout>
