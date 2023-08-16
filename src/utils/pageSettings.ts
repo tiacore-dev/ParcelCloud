094c86f589954f8512e675c64439f464e5d29106
ILIA TIMOFEEV <ilya48007@gmail.com>
2023-08-16 23:29:31 +0300

    fix

--- src/utils/pageSettings.ts
+++ src/utils/pageSettings.ts
@@ -1,5 +1,7 @@
 import { isMobile } from "./isMobile"
 
 export const minPageHeight = () => {
-    return isMobile() ? "calc(100vh - 161px)" : "calc(100vh - 185px)"
+    const scrollHeight = document.documentElement.clientHeight
+
+    return isMobile() ? `${scrollHeight - 161}px` : `${scrollHeight - 185}px`
 } 
